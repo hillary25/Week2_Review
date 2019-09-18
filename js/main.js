3f422c45285d5a29ecb831cfd183c53e90c8056f
Hillary <h_strong69707@fanshaweonline.ca>
2019-09-17 20:52:10 -0400

    Replacing files
    Switched out old files to new class files

---
+++ js/main.js
@@ -0,0 +1,45 @@
+import People from "./modules/dataObject.js";
+
+// IIFE - Immediately Invoiced Function Expression
+(() => {
+    console.log('fired!');
+    // grab the bio container and get ready to populate its content
+    const bioInfo = document.querySelector('.bio-wrapper').children;
+
+
+
+    function showProfData() {
+        // change data on the page here
+
+        // this is the label on the button (the name)
+        let currentProf = this.textContent;
+
+         // this refers to the heading tag (the first child of the bio-wrapper div)
+        bioInfo[0].textContent = People[currentProf].name;
+
+        // this refers to the paragraph tag (the second child of the bio-wrapper div)
+        bioInfo[1].textContent = People[currentProf].role;
+
+        bioInfo[2].textContent = People[currentProf].bio;
+
+        bioInfo[3].src = `images/${People[currentProf].avatar}`;
+        // bioInfo[2].src = "images/" + Person.avatar;
+    }
+
+    for (let prof in People) {
+        console.log(prof);
+
+        // create a button for every prof (every entry) in our object
+        let teamButton = document.createElement('button');
+
+        // set the button's label (text) to the prof name (the current entry in the object)
+        teamButton.textContent = prof;
+
+        // add some event handling so that when we click, we can show the right data
+        teamButton.addEventListener('click', showProfData);
+
+        // add the button to the top section on our web page
+        document.querySelector("#section1").appendChild(teamButton);
+    }
+
+})();
---
+++ js/modules/dataObject.js
@@ -0,0 +1,36 @@
+// objects are just collections of keys and values
+// remember to seperate the jets and values with a comma
+// and use a colon between the key and the value
+
+const Team = {
+    Trevor: {
+        name: "Trevor",
+        bio: "I don't know where I am...can you please help me",
+        role: "Professor",
+        avatar: "olaf.jpeg"
+    },
+
+    Joe: {
+        name: "Joe",
+        bio: "I love honey",
+        role: "Professor",
+        avatar: "pooh.jpeg"
+    },
+
+    Jarrod: {
+        name: "Jarrod",
+        bio: "I have more than one look!",
+        role: "Professor",
+        avatar: "zoolander.jpg"
+    },
+
+    Justin: {
+        name: "Justin",
+        bio: "Not sure where I came from",
+        role: "Coordinator",
+        avatar: "justin.jpeg"
+    }
+    
+}
+
+export default Team;
